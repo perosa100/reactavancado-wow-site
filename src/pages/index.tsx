ffd9beaa399d832be4f04806f3341b13c4c4aa1e
Patrick Perosa <patrickperosapp@gmail.com>
2021-07-11 08:34:30 -0400

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,7 @@ export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-// ATENÇÃO:
-// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
-
-// getStaticProps => gerar estático em build time
-// getServerSideProps => gerar via ssr a cada request
-// getInitialProps => gerar via ssr a cada request
-export function getServerSideProps() {
-  // faz lógica
-  // pode ser buscar dados numa API
-  // fazer calculo|leitura de context
-
-  // retorno dos dados
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
